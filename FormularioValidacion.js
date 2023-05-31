aa23298d5862ea213a55ceed0fef116983117fc7
Gabriel Melian <thegaboosx@gmail.com>
2023-05-31 10:19:27 -0300

    Se agregan formularios.

--- FormularioValidacion.js
+++ FormularioValidacion.js
@@ -9,3 +9,50 @@ for (var i=0; i<inputs.length; i++) {
         }
     })
 }
+
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("correo");
+const telefono = document.getElementById("telefono");
+const usuario = document.getElementById("usuario");
+const contra = document.getElementById("contra");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e=> {
+    e.preventDefault();
+    var numbers = /^[0-9]+$/;
+    let warnings = "";
+    let entrar = false;
+    parrafo.innerHTML = "";
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (nombre.value.length < 6) {
+        warnings += "El nombre es invalido.<br>";
+        entrar = true;
+    }
+    if (!regexEmail.test(correo.value)) {
+        warnings += "El email es invalido.<br>";
+        entrar = true;
+    }
+    if (contra.value.length < 8) {
+        warnings += "Contraseña invalida.<br>";
+        entrar = true;
+    }
+    if (!numbers.test(telefono.value)) {
+        warnings += "Telefono invalido.<br>";
+        entrar = true;
+    }
+    if (usuario.value.length < 2) {
+        warnings += "Usuario invalido.<br>";
+        entrar = true;
+    }
+
+    if (entrar) {
+        parrafo.innerHTML = warnings;
+    }
+    else {
+        parrafo.innerHTML = "Enviado";
+        form.reset();
+        
+    }
+
+})
